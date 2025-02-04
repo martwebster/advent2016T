ce2e522b3d4958d4744ec260821aa18dd71183e9
Martin Webster <mwebster1973@gmail.com>
2025-02-04 10:05:08 +0000

    day 12

---
+++ src/day12/day12.test.ts
@@ -0,0 +1,49 @@
+import '../utility/extensions';
+import { test, describe, expect } from 'vitest'
+import { readTestData } from '../utility/fileHelper';
+import {buildRegisters, copy, decrement, exampleFunction, increment, runProgram} from './day12';
+
+describe('day 12 - part1', () => {
+    test('simple commands', () => {
+        const registers = buildRegisters();
+        expect(registers.size).toBe(4);
+
+        // increment
+        increment(registers, "a");
+        expect(registers.get("a")).toBe(1)
+
+        // copy a into b
+        copy(registers, "a", "b");
+        expect(registers.get("b")).toBe(1)
+
+        copy(registers, "100", "c");
+        expect(registers.get("c")).toBe(100)
+
+        decrement(registers, "a");
+        expect(registers.get("a")).toBe(0)
+    })
+
+    test('sample', () => {
+        const data = readTestData('./src/day12/sample.txt');
+        const val = runProgram(data)
+        expect(val).toBe(42);
+    })
+
+    test('part1', () => {
+        const data = readTestData('./src/day12/input.txt');
+        const val = runProgram(data)
+        expect(val).toBe(318077);
+    })
+
+    test('part1', () => {
+        const data = readTestData('./src/day12/input.txt');
+        const val = runProgram(data)
+        expect(val).toBe(318077);
+    })
+
+    test('part2', () => {
+        const data = readTestData('./src/day12/input.txt');
+        const val = runProgram(data,1)
+        expect(val).toBe(9227731);
+    })
+})
---
+++ src/day12/day12.ts
@@ -0,0 +1,64 @@
+export const buildRegisters = (): Map<string, number> =>{
+    const map = new Map<string, number>();
+    map.set("a", 0)
+    map.set("b", 0)
+    map.set("c", 0)
+    map.set("d", 0)
+
+    return map;
+}
+
+export const increment = (registers: Map<string, number>, item: string) => {
+    const val = registers.get(item);
+    if (val == undefined) {
+        throw new Error(`increment ${item}`);
+    }
+    registers.set(item, val!+1);
+}
+export const decrement = (registers: Map<string, number>, item: string) => {
+    const val = registers.get(item);
+    if (val == undefined) {
+        throw new Error(`decrement ${item}`);
+    }
+    registers.set(item, val!-1);
+}
+export const copy = (registers: Map<string, number>, itemSource: string, itemDestination: string) => {
+    const val = registers.get(itemSource);
+    if (val == undefined) {
+        registers.set(itemDestination, Number(itemSource));
+    } else{
+        registers.set(itemDestination, val);
+    }
+}
+
+export const runProgram = (program: string[], ignitionValue?:number): number =>{
+    const registers = buildRegisters();
+
+    // part 2
+    if (ignitionValue != undefined) {
+        registers.set("c",ignitionValue)
+    }
+
+    let pos = 0;
+    while (pos < program.length){
+        const parts = program[pos].split(" ");
+        if (parts[0] == "cpy"){
+            copy(registers, parts[1], parts[2]);
+            pos += 1;
+        } else if (parts[0] == "dec"){
+            decrement(registers, parts[1]);
+            pos += 1;
+        } else if (parts[0] == "inc"){
+            increment(registers, parts[1]);
+            pos += 1;
+        } else {
+            const val = registers.get(parts[1]);
+            if (val ==0){
+                pos += 1;
+            } else{
+                pos += Number(parts[2]);
+            }
+        }
+    }
+    return registers.get("a")!
+}
